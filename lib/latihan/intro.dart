e9e18f0d9de10a56f86f54c5a61a06408e0284b0
faizaldr <fdr00zenc@gmail.com>
2026-06-23 12:16:46 +0700

    berubah

--- lib/latihan/intro.dart
+++ lib/latihan/intro.dart
@@ -24,7 +24,10 @@ void main(){
   a = 12;
   print(a.runtimeType); //melihat tipe data
   print('Hallo World, $nama ');
+
+  // tanda seru (!) memaksa konversi dari tipe data nullable menjadi non-nullable
+  String alamat2 = alamat!;
   print(alamat);
-  nilai = 100;
-  print(nilai);
+  nilai = int.tryParse('100')!;
+  print(nilai/7);
 }
