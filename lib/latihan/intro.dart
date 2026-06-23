1cc16d754f6615c8a0124742b7c0a7ddf88aafbe
faizaldr <fdr00zenc@gmail.com>
2026-06-23 12:02:38 +0700

    berubah

--- lib/latihan/intro.dart
+++ lib/latihan/intro.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
 
+// deklarasi
 String nama = 'Badu';
 int usia = 12;
 
+// tidak boleh deklarasi tanpa inisialisasi jika tidak nullable
+// String alamat;
+
+// bisa jika variable nullable :
+String? alamat;
+var a;
+
 void main(){
+  alamat="sidoarjo";
+  //tidak boleh mengubah usia menjadi null karena bukan nullable
+  // usia = null;
+
+  a = 'lulus SD';
+  a = 12;
+  print(a.runtimeType);
   print('Hallo World, $nama ');
+  print(alamat);
 }
