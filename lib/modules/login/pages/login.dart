2e966bdc1cb5f9c0d2c8dcf908dbd4717828e1be
faizaldr <fdr00zenc@gmail.com>
2026-06-23 16:12:28 +0700

    berubah

--- lib/modules/login/pages/login.dart
+++ lib/modules/login/pages/login.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatefulWidget{
+class LoginPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return _LoginPageState();
   }
 }
 
-class _LoginPageState extends State<LoginPage>{
+class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-   return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(height: 20),
+          Image.network(
+            "https://setda.sidoarjokab.go.id/images/profile/default.png"
+          ),
+          SizedBox(height: 20),
+          TextFormField(),
+          SizedBox(height: 20),
+          TextFormField(),
+          SizedBox(height: 20),
+          ElevatedButton.icon(
+            onPressed: () {},
+            label: Text("LOGIN"),
+            icon: Icon(Icons.login),
+          ),
+        ],
+      ),
+    );
   }
 }
