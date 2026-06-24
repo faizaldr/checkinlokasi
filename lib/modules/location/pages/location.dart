1fd218fce9b09c1067a54e5cea9c4bff6d6c0531
faizaldr <fdr00zenc@gmail.com>
2026-06-24 15:03:39 +0700

    update

--- lib/modules/location/pages/location.dart
+++ lib/modules/location/pages/location.dart
@@ -1,6 +1,8 @@
+import 'package:checkinlokasi/modules/location/data/location_api.dart';
+import 'package:checkinlokasi/modules/location/models/location_response_model.dart';
 import 'package:flutter/material.dart';
 
-class LocationPage extends StatefulWidget{
+class LocationPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return _LocationPageState();
@@ -8,8 +10,45 @@ class LocationPage extends StatefulWidget{
 }
 
 class _LocationPageState extends State<LocationPage> {
+  LocationResponse? _locationResponse;
+
+  @override
+  void initState() {
+    super.initState();
+    _initLocationResponse();
+  }
+
+  _initLocationResponse() async {
+    _locationResponse = await LocationApi().getLocations();
+    setState(() {
+      _locationResponse = _locationResponse;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-   return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text("Lokasi")),
+      body: _locationResponse == null
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: _locationResponse!.data!.length,
+              itemBuilder: (context, index) {
+                var data = _locationResponse!.data![index];
+                return Card(
+                  child: Column(
+                    children: [
+                      Text(
+                        "${data.placeName!} (${data.placeType!})",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text(data.comment!),
+
+                    ],
+                  ),
+                );
+              },
+            ),
+    );
   }
 }
