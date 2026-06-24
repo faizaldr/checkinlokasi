f2d3ad041ec1d64bbfde0cb92f3f2b19f0d4bb31
faizaldr <fdr00zenc@gmail.com>
2026-06-24 14:17:36 +0700

    update

--- lib/modules/location/data/location_api.dart
+++ lib/modules/location/data/location_api.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
+import 'package:checkinlokasi/consts/api.dart';
+import 'package:checkinlokasi/modules/location/models/location_response_model.dart';
 import 'package:checkinlokasi/modules/login/data/login_db.dart';
 import 'package:http/http.dart' as http;
+
 class LocationApi {
   final LoginDb _loginDb = LoginDb();
 
@@ -10,8 +15,25 @@ class LocationApi {
       'Accept': 'application/json',
     };
     if (token != null && token.isNotEmpty) {
-      headers['Authorization']='Bearer $token';
+      headers['Authorization'] = 'Bearer $token';
     }
     return headers;
+  }
+
+  Future<LocationResponse?> getLocations() async {
+    try {
+      final response = await http.get(
+        Uri.parse(BASE_TRACKING_URL),
+        headers: await _getHeaders(),
+      );
+      if(response.statusCode == 200){
+        final Map<String, dynamic> responseData= jsonDecode(response.body);
+        return LocationResponse.fromJson(responseData);
+      }else{
+        return null;
+      }
+    } catch (e) {
+      return null;
+    }
   }
 }
