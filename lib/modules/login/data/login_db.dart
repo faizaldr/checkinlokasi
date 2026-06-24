c325244170953047b4afa460dbbb2b2c09395a4d
faizaldr <fdr00zenc@gmail.com>
2026-06-24 11:36:03 +0700

    update

--- lib/modules/login/data/login_db.dart
+++ lib/modules/login/data/login_db.dart
@@ -4,8 +4,8 @@ import 'package:sqflite/sqflite.dart';
 class LoginDb {
   static Database? _database;
 
-  Future<Database> get database async{
-    if(_database !=null) return _database!;
+  Future<Database> get database async {
+    if (_database != null) return _database!;
     _database = await _initDB();
     return _database!;
   }
@@ -22,5 +22,29 @@ class LoginDb {
         );
       },
     );
+  }
+
+  Future<int> saveToken(String token) async {
+    final db = await database;
+    return await db.insert("tokens", {'token': token});
+  }
+
+  Future<String?> getToken() async {
+    final db = await database;
+    final List<Map<String, dynamic>> maps = await db.query('token', limit: 1);
+    if (maps.isNotEmpty) {
+      return (maps.first)['token'] as String;
+    }
+    return null;
+  }
+
+  Future<int> updateToken(String token) async {
+    final db = await database;
+    return await db.update('tokens', {'token': token});
+  }
+
+  Future<int> deleteToken(String token) async {
+    final db = await database;
+    return await db.delete('tokens');
   }
 }
